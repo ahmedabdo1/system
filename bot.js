cac84fbdc4fc1937cab51a1715e75a84956b4a4e
ahmedabdo1 <45934204+ahmedabdo1@users.noreply.github.com>
2018-12-17 15:45:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,23 +6,6 @@ const prefix = "!";
 
 
 
-client.on('ready', function(){
-    var ms = 100000 ;
-    var setGame = [`iSryBot BETA : V2 `,`&help - &inv`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
-    }, ms);100000
-
-
 
 
 
