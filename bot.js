ee59c53784ea4f456f7611333fd07bfe44df920f
ahmedabdo1 <45934204+ahmedabdo1@users.noreply.github.com>
2018-12-17 12:38:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,11 @@ const prefix = "!";
 
 
 
-
+client.on('guildMemberAdd', (member) => {
+    var channel = member.guild.channels.find('name', 'log');
+channel.send(`@${member.user.tag} تم اعطاء العضو رتبت ممبر`)
+member.addRole(member.guild.roles.find('name', 'members'));
+});
 
 
 
