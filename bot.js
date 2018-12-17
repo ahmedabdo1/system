2f344c03a4a802cd17134fdfda1e527b3331d210
ahmedabdo1 <45934204+ahmedabdo1@users.noreply.github.com>
2018-12-17 12:30:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,28 +44,6 @@ voiceChannel.leave();
 
 
 
-client.on('message', message => {
-             if (!message.channel.guild) return;
-      if (message.author.bot) return;
-
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-  
-  if (command === 'invite') {
-    message.guild.fetchInvites().then(invs => {
-      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-    return message.reply(`**${inviteCount}: عدد الاشخاص الذي دعوتهم هو**`)
-
-});
-}});
-
-
-
-
 
 
 
