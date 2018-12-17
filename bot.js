1e625625279db87148f89e7d13d43f3c477c9f0b
ahmedabdo1 <45934204+ahmedabdo1@users.noreply.github.com>
2018-12-17 11:13:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,22 @@ const snekfetch = require("snekfetch");
 
 
 
-
+client.on('message', message => {
+    if (message.content.startsWith("افتار")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
 
 
 
