064917e1c20d522825aac8090be52d66f4f49f0c
ahmedabdo1 <45934204+ahmedabdo1@users.noreply.github.com>
2018-12-20 10:20:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,24 +13,6 @@ const prefix = "!";
 
 
 
-client.on('message', message => {
-  var prefix = "!"; /// غير البرفيكس
-  
-if (message.author.bot) return;
-if (!message.content.startsWith(prefix)) return;
-   
-let command = message.content.split(" ")[0];
-command = command.slice(prefix.length);
-   
-let args = message.content.split(" ").slice(1);
-   
-if (command === "say") {
-message.delete()
-  message.channel.sendMessage(args.join(" ")).catch(console.error);
-}
-
-
-
 
 
 
