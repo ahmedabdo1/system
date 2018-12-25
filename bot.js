560b20b4c56a5ecf54b2efc387609735af39eea6
ahmedabdo1 <45934204+ahmedabdo1@users.noreply.github.com>
2018-12-24 23:29:09 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "NDIyMDM3NzQwNTkxMzgyNTI4.DYV8zA.6vBJjF7Op9T_bH_YOwRMXqwh9vc";
+const discord_token = "NTI0Mzk3ODE4MjQ1MjgzODUw.DwNtJw.VX1iFF9KXEyEWO2-9zt_g2C2j5k";
 var table = require('table').table
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
